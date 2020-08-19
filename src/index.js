368e0d1da7c62f4cab481f133f331f3bc9aa5b19
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-08-19 13:08:20 +0200

    component

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Create a new component. This component should replace some HTML
+const App = function() {
+    return <div>Hi!</div>
+}
+
+//Take this component generated html and put it on the DOM
