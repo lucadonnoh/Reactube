fd576435ccff1e644796a0b2bdff86cbb961fa09
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-08-27 21:29:20 +0200

    App rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create a new component. This component should replace some HTML
-const App = function() {
+const App = () => {
     return <div>Hi!</div>
 }
 
 //Take this component generated html and put it on the DOM
+
+ReactDOM.render(<App />, document.querySelector('.container'));
