72a71b5685a5523830ea9a07a00c385042a4fd6b
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-09-05 18:48:22 +0200

    searchbar component initalizated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDvwfcg4Kb_xCB7b5CjH0cA0olhubxcWO0';
 
 // Create a new component. This component should replace some HTML
 const App = () => {
-    return <div>Hi!</div>
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
 }
 
 //Take this component generated html and put it on the DOM
