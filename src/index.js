20bf8175cd6f38e317cc0dad7e9b708fa6820bbf
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-09-11 16:54:52 +0200

    oninputchange search

--- src/index.js
+++ src/index.js
@@ -16,8 +16,12 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-            this.setState({ 
+        this.videoSearch('donnoh');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
          });
@@ -29,9 +33,9 @@ class App extends Component {
     {
         return (
         <div>
-            <SearchBar />
+            <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
             <VideoDetail video={this.state.selectedVideo}/>
-            <VideoList 
+            <VideoList
                 onVideoSelect={selectedVideo => this.setState({selectedVideo})}
                 videos={this.state.videos} />
         </div>
