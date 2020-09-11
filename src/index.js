d786dfb117ae8005a2cf87db532812f480a807ca
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-09-11 14:47:24 +0200

    video list

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import ReactDOM, { render } from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDvwfcg4Kb_xCB7b5CjH0cA0olhubxcWO0';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
+
     }
 
     render()
@@ -20,6 +23,7 @@ class App extends Component {
         return (
         <div>
             <SearchBar />
+            <VideoList videos={this.state.videos} />
         </div>
         );
     }
